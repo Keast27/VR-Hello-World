f59792013a164d81d128465252ff0f236568c2ad
Eddie Brazier <edwinbrazier01@gmail.com>
2020-11-03 14:43:04 -0500

    added basic wine spilling and date happiness

---
+++ Assets/Scripts/DatingSim/DateHappiness.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Happiness
+{
+    Angry,
+    Neutral,
+    Happy
+}
+
+
+public class DateHappiness : MonoBehaviour
+{
+    //current happiness values
+    private int currentHappiness = 5;
+    private Happiness currentState = Happiness.Neutral;
+
+    //materials
+    public List<Material> materials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check happiness levels
+        if(currentHappiness <= 3 && currentState != Happiness.Angry)
+        {
+            //change material and current state
+            GetComponent<MeshRenderer>().material = materials[0];
+            currentState = Happiness.Angry;
+        }
+        else if(currentHappiness >= 7 && currentState != Happiness.Happy)
+        {
+            //change material and current state
+            GetComponent<MeshRenderer>().material = materials[1];
+            currentState = Happiness.Happy;
+        }
+        else if(currentHappiness > 3 && currentHappiness < 7 && currentState != Happiness.Neutral)
+        {
+            //change material and current state
+            GetComponent<MeshRenderer>().material = materials[2];
+            currentState = Happiness.Neutral;
+        }
+
+        //don't let happiness go below zero or above ten
+        if(currentHappiness < 0)
+        {
+            currentHappiness = 0;
+        }
+        else if(currentHappiness > 10)
+        {
+            currentHappiness = 10;
+        }
+    }
+
+    public void DecreaseHappiness(int decrement)
+    {
+        currentHappiness -= decrement;
+    }
+
+    public void IncreaseHappiness(int increment)
+    {
+        currentHappiness += increment;
+    }
+}
---
+++ Assets/Scripts/DatingSim/PlayerGlass.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGlass : MonoBehaviour
+{
+    //particle system
+    private ParticleSystem wineParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wineParticles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if the up vector of this object has a negative y component
+        if(transform.up.y < 0 && wineParticles.isPlaying != true)
+        {
+            //spill the drink and decrease date happiness
+            wineParticles.Play();
+
+            //decrease date happiness
+            GameObject.FindGameObjectWithTag("Date").GetComponent<DateHappiness>().DecreaseHappiness(1);
+        }
+        else if(transform.up.y > 0 && wineParticles.isPlaying == true)
+        {
+            //stop the wine
+            wineParticles.Stop();
+        }
+    }
+}
